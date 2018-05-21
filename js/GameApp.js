66aea06d8681f1a95e8a51f1cc76b83c695a20eb
MariahAdams <mariah.adams23@yahoo.com>
2018-05-21 16:33:21 -0700

    added comments for items to be added

--- js/GameApp.js
+++ js/GameApp.js
@@ -1,4 +1,5 @@
 /* exported GameApp */
+/* globals ScreenDisplay */
 'use strict';
 
 const gameAppTemplate = document.getElementById('game-app-template');
@@ -6,13 +7,16 @@ const gameAppTemplate = document.getElementById('game-app-template');
 class GameApp {
     constructor() {
         //initialization logic to be added later
+        //link to user profile
+        //link to alchemon data
+        //link to location data
     }
 
     render() {
         const dom = gameAppTemplate.content;
 
-        //template sections to be added later:
-        //screen display
+        //template sections to be added:
+        //screen display section:
         const screenDisplaySection = dom.getElementById('screen-display-section');
         const screenComponent = new ScreenDisplay();
         screenDisplaySection.appendChild(screenComponent.render());
