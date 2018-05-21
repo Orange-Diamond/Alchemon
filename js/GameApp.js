8d189543227f6a662025c7cce14c20ccfce4e8ec
MariahAdams <mariah.adams23@yahoo.com>
2018-05-21 16:15:19 -0700

    initial structure of GameApp class established

---
+++ js/GameApp.js
@@ -0,0 +1,20 @@
+/* exported GameApp */
+'use strict';
+
+const gameAppTemplate = document.getElementById('game-app-template');
+
+class GameApp {
+    constructor() {
+        //initialization logic to be added later
+    }
+
+    render() {
+        const dom = gameAppTemplate.content;
+
+        //template sections to be added later:
+        //screen display
+        //text/input display 
+
+        return dom;
+    }
+}
