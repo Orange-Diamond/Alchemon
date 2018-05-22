147ba3836ec860019423385ff2d31e85f2b5100d
MariahAdams <mariah.adams23@yahoo.com>
2018-05-22 11:32:22 -0700

    added obj literals for location backgrounds. Current images linked: main map and lab interior.

---
+++ js/Locations.js
@@ -0,0 +1,21 @@
+/* exported locationBgData */
+'use strict';
+
+let locationBgData = [
+    {
+        id: 'main-map-bg',
+        src: 'images/main-map-bg.jpg'
+    },
+    {
+        id: 'lab-interior',
+        src: 'images/lab-interior.jpg'
+    },
+    // {
+    //     id: 'gym-interior',
+    //     src: 'images/'
+    // },
+    // {
+    //     id: 'home-interior',
+    //     src: 'images/'
+    // },
+];
