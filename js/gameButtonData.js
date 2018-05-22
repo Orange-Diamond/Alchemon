a5f8194178c674575d9daf9537cacb70e0ebc3eb
MariahAdams <mariah.adams23@yahoo.com>
2018-05-22 11:17:20 -0700

    added gameButtonData.js and started scaffolding for obj literals

---
+++ js/gameButtonData.js
@@ -0,0 +1,55 @@
+/*exported pokeballData, docBookData, buildingData, trainerData */
+'use strict';
+
+
+let pokeballData = [
+    {
+        id: 'pokeball',
+        src: 'images/'
+    },
+];
+
+let docBookData = [
+    {
+        id: 'html-doc',
+        src: 'images/'
+    },
+    {
+        id: 'css-doc',
+        src: 'images/'
+    },
+    {
+        id: 'js-doc',
+        src: 'images/'
+    },
+];
+
+let buildingData = [
+    {
+        id: 'gym',
+        src: 'images/'
+    },
+    {
+        id: 'lab',
+        src: 'images/'
+    },
+    {
+        id: 'home',
+        src: 'images/'
+    },
+];
+
+let trainerData = [
+    {
+        id: '',
+        src: 'images/'
+    },
+    {
+        id: '',
+        src: 'images/'
+    },
+    {
+        id: '',
+        src: 'images/'
+    },
+];
