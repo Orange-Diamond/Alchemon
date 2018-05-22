85bf9b7d77751c98718a7fd475cc2447b3612dd3
Chris Golden <chrgolden@gmail.com>
2018-05-21 21:05:54 -0700

    buttons displaying in with for loop

---
+++ js/Button.js
@@ -0,0 +1,12 @@
+/*exported  */
+'use strict';
+const buttonTemplate = document.getElementById('button-template');
+class Button {
+    
+    render() {
+        const dom = buttonTemplate.content.cloneNode(true);
+        
+        return dom; 
+    }
+
+}
