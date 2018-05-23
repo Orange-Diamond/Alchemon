4d856774f97dfc2034b61b3429562616b57ee2de
MariahAdams <mariah.adams23@yahoo.com>
2018-05-22 17:34:58 -0700

    intro app scaffolding redone

--- js/IntroApp.js
+++ js/IntroApp.js
@@ -9,5 +9,19 @@ class IntroApp {
 
     }
 
-    
+    render() {
+        const dom = introAppTemplate.content.cloneNode(true);
+
+        this.labScreenArea = dom.getElementById('lab-screen-area');
+        const labScreenComponent = new LabScreenDisplay(this.buttonList, (buttonClicked) => {
+
+        });
+
+        const labDialogueArea = dom.getElementById('lab-dialog-area');
+        const labDialogueComponent = new LabDialogueDisplay;
+        labDialogueArea.appendChild(labDialogueComponent.render());
+        this.labScreenArea.appendChild(labScreenComponent.render());
+
+        return dom;
+    }
 }
