7c8cea45ac7c4daa94a3ba6f8ba43b95f474b841
MariahAdams <mariah.adams23@yahoo.com>
2018-05-22 17:13:37 -0700

    scaffolding for intro app complete

---
+++ js/LabScreen.js
@@ -0,0 +1,18 @@
+/*  */
+/*  */
+'use strict';
+
+const labScreenAreaTemplate = document.getElementById('lab-screen-area-template');
+
+class LabScreenDisplay {
+    constructor() {
+
+    }
+
+    render() {
+        const dom = labScreenAreaTemplate.content.cloneNode(true);
+        
+        return dom;
+    }
+
+}
