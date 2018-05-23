ecf2c1521c88afd883740ea6c31245bb48cfde75
Antreo Pukay <acrosswalker@yahoo.com>
2018-05-23 12:03:27 -0700

    working on table

---
+++ js/Results.js
@@ -0,0 +1,18 @@
+/* globals rowResults */
+/* exported rowResults */
+
+const resultsColumn = [playerName, alchemonCreature, wins];
+let playerName = 0;
+
+function playerResults() {
+    for(let i = 0; i < 1; i++)
+    
+    const table = document.getElementsById('player');
+    
+    tRow = table.createElement('tr'); 
+for(let i in resultsColumn) {
+    
+}
+    
+}
+console.log('tRow', tRow);
