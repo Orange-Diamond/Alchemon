1eb7bdd9ea768f1d724dbbb6029ed5756522fbe6
Chris Golden <chrgolden@gmail.com>
2018-05-23 17:56:59 -0700

    computers are now linking to websites

--- js/GameApp.js
+++ js/GameApp.js
@@ -1,5 +1,5 @@
-/* exported randomize  GameApp */
-/* globals ScreenDisplay mapRefresh homeData trainerData buildingData DialogueDisplay*/
+/* exported  randomize  GameApp */
+/* globals mapRefresh docBookData ScreenDisplay mapRefresh homeData trainerData buildingData DialogueDisplay*/
 'use strict';
 
 const gameAppTemplate = document.getElementById('game-app-template');
@@ -16,30 +16,30 @@ class GameApp {
         const dom = gameAppTemplate.content.cloneNode(true);
         this.screenArea = dom.getElementById('screen-area');
         const screenComponent = new ScreenDisplay(this.buttonList, (buttonClicked) => {
-        // define loss criteria then do = new ScreenDisplay()
+            let dialogue = document.getElementById('dialogue');
+            console.log(buttonClicked);
 
-        let dialogue = document.getElementById('dialogue');
-        console.log(buttonClicked);
-
-        if(trainerData.includes(buttonClicked)){
-            const battleResult = gameApp.randomize(this.result);
+            if(trainerData.includes(buttonClicked)){
+                const battleResult = gameApp.randomize(this.result);
      
-            console.log('Clicked trainer');
-            dialogue.textContent ='You battled ' + buttonClicked.id + '! and you ' + battleResult;
-            if(this.lives === 0){
-                this.screenArea.style.backgroundImage = 'url(\'' + 'images/loser.jpg' + '\')';
-
-                screenComponent.update(buttonClicked.buttons);
-                
+                console.log('Clicked trainer');
+                dialogue.textContent = 'You battled ' + buttonClicked.id + '! and you ' + battleResult;
+                if(this.lives === 0){
+                    this.screenArea.style.backgroundImage = 'url(\'' + 'images/loser.jpg' + '\')';
+                    screenComponent.update(buttonClicked.buttons);
+                }
             }
-        }
-        else if(homeData.includes(buttonClicked)){
-            dialogue.textContent = 'NO TOUCHING! JUST LOOKING.... UNLESS YOU HAS SNAKS?';
-        }
-        else {
-            this.screenArea.style.backgroundImage = 'url(\'' + buttonClicked.bgSrc + '\')';
-            screenComponent.update(buttonClicked.buttons);
-        }
+            else if(homeData.includes(buttonClicked)){
+                dialogue.textContent = 'NO TOUCHING! JUST LOOKING.... UNLESS YOU HAS SNAKS?';
+            }
+            else if(docBookData.includes(buttonClicked)){
+                window.open(buttonClicked.link);
+                console.log('clicked ' + buttonClicked.id);
+            }
+            else {
+                this.screenArea.style.backgroundImage = 'url(\'' + buttonClicked.bgSrc + '\')';
+                screenComponent.update(buttonClicked.buttons);
+            }
         });
 
         const dialogueArea = dom.getElementById('dialog-area');
@@ -50,26 +50,22 @@ class GameApp {
         return dom;
     }
 
-    randomize (result) {
+    randomize(result) {
 
         var randomNum = Math.floor(((Math.random()) * 20));
         const randomPlayerScore = randomNum;
         this.result = result;
         if(randomPlayerScore < 6) {
             this.lives--;
-            console.log('loss'+ this.lives );
+            console.log('loss' + this.lives);
             
             return result = ' LOST!  ';
         } else {
             this.wins++;
-
-            console.log('win'+ this.wins );
+            console.log('win' + this.wins);
             return result = ' WON!  ';
-
         }
-            
     }
-
 }
 
 
@@ -79,6 +75,5 @@ function mapRefresh(){
     // const screenArea = dom.getElementById('screen-area');
     // console.log(screenArea.backgroundImage);
     // screenArea.style.backgroundImage = 'url(\'images/map-area-bg.jpg\')';
-    
     // console.log('clicked');
 }
