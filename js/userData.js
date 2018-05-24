b4bf790c30286fa216ec37a580dab3c3a1bdb826
MariahAdams <mariah.adams23@yahoo.com>
2018-05-24 07:41:18 -0700

    user object is counting wins and lives lost. local storage works but needs a clear data button.

--- js/userData.js
+++ js/userData.js
@@ -1,29 +1,37 @@
 /* exported userData, initUserData, clearUserData */
 'use strict';
 
-const userProfile = window.localStorage.getItem('userData');
+// const userProfile = window.localStorage.getItem('userData');
 
-window.onbeforeunload = () => {
-    window.localStorage.setItem('userData', JSON.stringify(userData));
-};
+// window.onbeforeunload = () => {
+//     window.localStorage.setItem('userData', JSON.stringify(userData));
+// };
 
-let userData;
-if(userProfile) {
-    userData = JSON.parse(userProfile);
-}
-else {
-    initUserData();
-}
+// let userData;
+// if(userProfile) {
+//     userData = JSON.parse(userProfile);
+// }
+// else {
+//     initUserData();
+// }
 
-function initUserData() {
-    userData = [
-        {
-            name: '',
-            lives: 0,
-            wins: 0
-        }
-    ];
-}
+// function initUserData() {
+//     userData = [
+//         {
+//             playerName: '',
+//             lives: 3,
+//             wins: 0
+//         }
+//     ];
+// }
+
+let userData = [
+    {
+        playerName: '',
+        lives: 3,
+        wins: 0
+    }
+]
 
 console.log(userData);
 
